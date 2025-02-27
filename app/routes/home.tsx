7e66e4a1b2617cc6fb2ebe8ad3ff6c59c683697f
Saul Lee <solleedata@gmail.com>
2025-02-27 23:27:01 +0900

    refactor: simplify Home component by removing loading and error handling

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,9 +1,7 @@
+import CTAButton from "~/components/CTAButton";
+import Txt from "~/components/Txt";
 import { useUser } from "~/features/user/hooks";
 import type { Route } from "./+types/home";
-import Loading from "~/components/Loading";
-import Error from "~/components/Error";
-import Txt from "~/components/Txt";
-import CTAButton from "~/components/CTAButton";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,17 +11,7 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  const { user, userLoading, userError } = useUser();
-
-  if (userLoading) {
-    return <Loading message="사용자 정보를 불러오고 있습니다..." />;
-  }
-
-  if (userError) {
-    return (
-      <Error message="사용자 정보를 불러올 수 없습니다. 새로고침하고 다시 시도해주세요" />
-    );
-  }
+  const { user } = useUser();
 
   return (
     <>
